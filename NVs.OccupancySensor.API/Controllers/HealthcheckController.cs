6425bc4ee885cf081a3a8f072d729c641884baa2
Nikita Senkov <nvsnkv@live.com>
2020-12-07 23:13:57 +0300

    Add project files.

---
+++ NVs.OccupancySensor.API/Controllers/HealthcheckController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace NVs.OccupancySensor.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthcheckController : ControllerBase
+    {
+        private readonly ILogger<HealthcheckController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HealthcheckController(ILogger<HealthcheckController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.Log(LogLevel.Trace, "Healthcheck called");
+            return $"OK (API version: {_configuration["Version"]})";
+        }
+    }
+}
