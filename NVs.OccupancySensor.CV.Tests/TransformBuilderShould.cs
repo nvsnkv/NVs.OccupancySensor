ff8d422110bcbe99e9a113b19a74239245bd1421
Nikita Senkov <nvsnkv@live.com>
2021-01-03 22:54:14 +0300

    Tests for transformer

---
+++ NVs.OccupancySensor.CV.Tests/TransformBuilderShould.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NVs.OccupancySensor.CV.Transformation;
+using Xunit;
+
+namespace NVs.OccupancySensor.CV.Tests
+{
+    public class TransformBuilderShould
+    {
+        private readonly Mock<ILogger<ImageTransformer>> logger = new Mock<ILogger<ImageTransformer>>();
+
+        private ILogger<ImageTransformer> GetLogger() => logger.Object;
+
+        [Fact]
+        public void BuildTransformerWithCorrectSequenceOfActions()
+        {
+            DateTime? invokedA = null;
+            DateTime? invokedB = null;
+
+            var transformer = new ImageTransformBuilder(GetLogger)
+                .Append((Image<Rgb, byte> _) =>
+                {
+                    invokedA = DateTime.Now;
+                    return new Image<Gray, byte>(100, 100);
+                }).Append((Image<Gray, byte> _) =>
+                {
+                    invokedB = DateTime.Now;
+                    return new Image<Rgb, byte>(100, 100);
+                }).ToTransformer();
+
+            transformer.Transform(new Image<Rgb, byte>(100, 100));
+            
+            Assert.NotNull(invokedA);
+            Assert.NotNull(invokedB);
+            
+            Assert.True(invokedA < invokedB);
+        }
+        
+        [Fact]
+        public void ThrowInvalidOperationExceptionIfTransformTypesDoesNotMatch()
+        {
+            var builder = new ImageTransformBuilder(GetLogger)
+                .Append((Image<Rgb, byte> _) => new Image<Gray, byte>(100, 100));
+
+            Assert.Throws<InvalidOperationException>(() =>
+                builder.Append((Image<Rgb, byte> _) => new Image<Rgb, byte>(100, 100)));
+        }
+    }
+}
---
+++ NVs.OccupancySensor.CV.Tests/Utils/TestTransform.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using NVs.OccupancySensor.CV.Transformation;
+
+namespace NVs.OccupancySensor.CV.Tests.Utils
+{
+    sealed class TestTransform : ITransform
+    {
+        private readonly Func<TestTransform, TestTransform> copyFunc;
+
+        public TestTransform(Func<TestTransform, TestTransform> copyFunc = null)
+        {
+            this.copyFunc = copyFunc ?? (t => t);
+        }
+        
+        public DateTime? ApplyInvokedOn {get; private set; }
+        public bool Disposed { get; private set; }
+        
+        public void Dispose()
+        {
+            Disposed = true;
+        }
+
+        public object Apply(object input)
+        {
+            ApplyInvokedOn = DateTime.Now;
+            Task.Delay(TimeSpan.FromMilliseconds(10)).Wait();
+            return input;
+        }
+
+        public ITransform Copy()
+        {
+            return copyFunc(this);
+        }
+    }
+}
