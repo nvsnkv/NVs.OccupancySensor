bdc2c36ec79c3aecc3cae35a230a2d25a92974cb
Nikita Senkov <nvsnkv@live.com>
2021-01-05 13:30:07 +0300

    added test for synchronization transformer

--- NVs.OccupancySensor.CV.Tests/TransformBuilderShould.cs
+++ NVs.OccupancySensor.CV.Tests/TransformBuilderShould.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Emgu.CV;
 using Emgu.CV.Structure;
 using Microsoft.Extensions.Logging;
@@ -33,13 +36,13 @@ namespace NVs.OccupancySensor.CV.Tests
                 }).ToTransformer();
 
             transformer.Transform(new Image<Rgb, byte>(100, 100));
-            
+
             Assert.NotNull(invokedA);
             Assert.NotNull(invokedB);
-            
+
             Assert.True(invokedA < invokedB);
         }
-        
+
         [Fact]
         public void ThrowInvalidOperationExceptionIfTransformTypesDoesNotMatch()
         {
@@ -48,6 +51,36 @@ namespace NVs.OccupancySensor.CV.Tests
 
             Assert.Throws<InvalidOperationException>(() =>
                 builder.Append((Image<Rgb, byte> _) => new Image<Rgb, byte>(100, 100)));
+        }
+
+        private volatile int seq = 0;
+
+        [Fact]
+        public void AddSynchronizationWhenRequested()
+        {
+
+            Image<Rgb, byte> LongRunningTransform(Image<Rgb, byte> src)
+            {
+                Task.Delay(200).Wait();
+                var image = new Image<Rgb, byte>(10, 10)
+                {
+                    Data = { [0, 0, 0] = (byte)Interlocked.Increment(ref seq) }
+                };
+
+                return image;
+            }
+
+            var builder = new ImageTransformBuilder(GetLogger).Append((Func<Image<Rgb, byte>, Image<Rgb, byte>>)LongRunningTransform).Synchronized();
+            var transformer = builder.ToTransformer();
+
+            var results = Enumerable.Range(0, 3)
+                .AsParallel()
+                .Select(_ => transformer.Transform(new Image<Rgb, byte>(1, 1)))
+                .ToList();
+
+            Assert.Equal(1, results[0].Data[0, 0, 0]);
+            Assert.Equal(1, results[1].Data[0, 0, 0]);
+            Assert.Equal(1, results[2].Data[0, 0, 0]);
         }
     }
 }
