2e2e054d94da9f02ad6fbe22c3584226e7f9b456
Nikita Senkov <nvsnkv@live.com>
2021-01-08 16:07:42 +0300

    added serilog

--- NVs.OccupancySensor.API/Program.cs
+++ NVs.OccupancySensor.API/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace NVs.OccupancySensor.API
 {
@@ -13,11 +16,35 @@ namespace NVs.OccupancySensor.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(new RenderedCompactJsonFormatter(), 
+                    "/var/log/occupancy_sensor/log.ndjson", 
+                    buffered:true, 
+                    flushToDiskInterval: TimeSpan.FromMinutes(1), 
+                    rollingInterval: RollingInterval.Day, 
+                    retainedFileCountLimit:10)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Application start-up failed!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
