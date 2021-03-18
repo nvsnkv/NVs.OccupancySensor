63f4649260f4e15c3e8f9fa892ba1cae40128a17
nvsnkv <nvsnkv@live.com>
2021-03-18 14:15:31 +0300

    Statefull correction strategy

---
+++ NVs.OccupancySensor.CV/Detection/Correction/IStatefulCorrectionStrategy.cs
@@ -0,0 +1,12 @@
+namespace NVs.OccupancySensor.CV.Detection.Correction
+{
+    public interface IStatefulCorrectionStrategy : ICorrectionStrategy 
+    {
+        void Load();
+        
+        void Save();
+
+        void Reset();
+    }
+
+}
